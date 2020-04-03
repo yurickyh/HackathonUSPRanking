e78ffe3b7ae4294070605cbd0779b2a43f2a1071
yurickyh <yurickyussuke@hotmail.com>
2020-04-02 23:42:35 -0300

    Refactor the code to change columns according to the header. Refactor code to facilitate the definition of the filename.
    Signed-off-by: yurickyh <yurickyussuke@hotmail.com>

--- scripts/GenerateFormattedData.py
+++ scripts/GenerateFormattedData.py
@@ -3,6 +3,7 @@ import re
 import warnings
 
 HEADER_MAP = {
+    'Edição': 'Edition',
     'Carimbo de data/hora': 'Datetime',
     'ID': 'id',
     'Selecionado': 'IsSelected',
@@ -12,18 +13,22 @@ HEADER_MAP = {
     'Em que ano você está?': 'GraduationYear',
     'Você já participou de quantos hackathons?': 'NHackathon',
     'Qual a sua especialidade?': 'Role',
+    'Quais as suas especialidades?': 'Roles',
     'Você já tem equipe formada?': 'HasTeam',
     'Se sim, conte-nos quem são:': 'TeamMembers',
-    'Time': 'TeamId'
+    'Time': 'TeamId',
+    'Em que universidade você estuda (ou estudou)?': 'University',
 }
 
 ADDITIONAL_COLUMNS = ['TeamSize']
+
 
 def editCollege(college: str) -> str:
     if college.find('(') > 0 and college.find(')') > 0:
         pattern = '\((.*?)\)'
         return re.search(pattern, college).group(1)
     return college
+
 
 def editGraduationYear(gradYear: str) -> str:
     gradNumbers = re.findall(r'\d+', gradYear)
@@ -32,16 +37,20 @@ def editGraduationYear(gradYear: str) -> str:
     warnings.warn('Unable to extract the gradYear=[%s]' % gradYear, Warning)
     return gradYear
 
+
 def getNumberTeamMembers(teamMembers: str) -> int:
     return len(teamMembers.split(','))
 
-def writeCsv(header: list, rows: list, directory: str):
-    newFile = open('../{directory}/2018.1-FormattedRegisteredFile.csv'.format(directory=directory), 'w')
+
+def writeCsv(header: list, rows: list, directory: str, edition: str):
+    filename = '../{directory}/{edition}-FormattedRegistrationFile.csv'.format(directory=directory, edition=edition)
+    newFile = open(filename, 'w')
 
     with newFile:
         csv_writer = csv.writer(newFile)
         csv_writer.writerow(getFormattedHeader(header))
         csv_writer.writerows(rows)
+
 
 def getFormattedHeader(header: list) -> list:
     newHeader = []
@@ -50,23 +59,37 @@ def getFormattedHeader(header: list) -> list:
     newHeader.extend(ADDITIONAL_COLUMNS)
     return newHeader
 
+def getIndexByColumn(header: list, column: str) -> int:
+    for i in range(len(header)):
+        if HEADER_MAP[header[i]] == column:
+            return i
+    raise ValueError("Impossible to get index for column=[{column}]".format(column=column))
+
 def main():
-    directory = 'data/2018.1'
-    filePath = '../{directory}/2018.1 - Inscritos.csv'.format(directory=directory)
+    edition = '2018.1'
+    directory = 'data/{edition}'.format(edition=edition)
+    filePath = '../{directory}/{edition} - Inscritos.csv'.format(directory=directory, edition=edition)
 
     with open(filePath) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter = ';')
+        csv_reader = csv.reader(csv_file, delimiter=';')
 
         header = next(csv_reader)
 
         newRows = []
 
         for row in csv_reader:
-            row[4] = editCollege(row[4])
-            row[6] = editGraduationYear(row[6])
-            row.append(getNumberTeamMembers(row[10]))
+            indexCollege = getIndexByColumn(header, 'College')
+            row[indexCollege] = editCollege(row[indexCollege])
+
+            indexGradYear = getIndexByColumn(header, 'GraduationYear')
+            row[indexGradYear] = editGraduationYear(row[indexGradYear])
+
+            indexTeamMembers = getIndexByColumn(header, 'TeamMembers')
+            row.append(getNumberTeamMembers(row[indexTeamMembers]))
+
             newRows.append(row)
 
-        writeCsv(header, newRows, directory)
+        writeCsv(header, newRows, directory, edition)
+
 
 main()
